79dc5fdb58f7adf39b13738c6aaea875411d49b9
Jonnie-Dev <jtech2096@gmail.com>
2023-02-24 10:34:21 -0800

    task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle
+ * @size: size of the traingle
+ */
+
+void print_triangle(int size)
+{
+	int row, spaces, hashes;
+
+	for (row = 1; row <= size; row++)
+	{
+		for (spaces = size - row; spaces >= 1; spaces--)
+		{
+			_putchar(' ');
+		}
+		for (hashes = 1; hashes <= row; hashes++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
